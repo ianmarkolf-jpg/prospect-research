92b37eb1aec93bbf2df078f36e536a0e9fc517fc
ianmarkolf-jpg <ianmarkolf@gmail.com>
2026-03-14 14:24:04 -0500

    Add Sheet ID

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   APPS_SCRIPT_URL: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',
   GOOGLE_CLIENT_ID: '49909057660-giejf63ekv0p7kaip313sikl8jnab9n6.apps.googleusercontent.com',
-  SHEET_ID: 'YOUR_GOOGLE_SHEET_ID',
+  SHEET_ID: '10betkhjKVEBoN_Mfk7BfivQGvpGthklcYmK1cfwEpug',
   ANTHROPIC_API_KEY: '',
 };
