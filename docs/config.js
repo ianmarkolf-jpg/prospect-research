6edf3dcdfdf42febdb13198dadd4b573a9b5dd60
ianmarkolf-jpg <ianmarkolf@gmail.com>
2026-03-15 06:07:52 -0500

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzqxBAEexRsZqSrs4B5Ru7v2HEar8VL71tuQMCAgjkJBDpcXTtFnp1vCnGGHGL9b-sUGw/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzmJi0nRxBXaDNV1oyAfItZA7pKwozDN36K4KHRSdKPv5SxErX54sgZiA8YKUNwI2wp/exec',
   GOOGLE_CLIENT_ID: '49909057660-giejf63ekv0p7kaip313sikl8jnab9n6.apps.googleusercontent.com',
   SHEET_ID: '10betkhjKVEBoN_Mfk7BfivQGvpGthklcYmK1cfwEpug',
   ANTHROPIC_API_KEY: '',
